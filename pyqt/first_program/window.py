40bbe1c2b2c735e77f9e02c536a43aa6075a9c26
Michael Angelo R. Cas <marc@csserver2.(none)>
2011-07-20 17:13:50 +0800

    window.py

--- pyqt/first_program/window.py
+++ pyqt/first_program/window.py
@@ -8,29 +8,34 @@ from PyQt4 import QtGui, QtCore
 
 
 
+class window(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
 
-class Icon(QtGui.QWidget):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-class QuitButton(QtGui.QWidget):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+   
+			
 
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('greyhoundz')
         self.setWindowIcon(QtGui.QIcon('grey.jpg'))
+        self.resize(250,150)
+        self.center()
+
+    def center(self):
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        size =  self.geometry()
+        self.move((screen.width()-self.width())/2, (screen.height()-self.height())/2)
         
         quit = QtGui.QPushButton('Close', self)
-        quit.setGeometry(10, 10, 60, 35)
-
+        quit.setGeometry(0, 0, 60, 35)
+        quitgeo = quit.geometry();
+        quit.move((size.width()/2) - (quitgeo.width()/2), (size.height()/2) - (quitgeo.height()/2))
         self.connect(quit, QtCore.SIGNAL('clicked()'),
             QtGui.qApp, QtCore.SLOT('quit()'))
 
 
 
 app = QtGui.QApplication(sys.argv)
-qb = QuitButton()
-icon = Icon()
+qb = window()
 qb.show()
-icon.show()
 sys.exit(app.exec_())
