943bd6172c6d765c87b16c81fb73e1a7dfbc5bfe
Michael Angelo R. Cas <marc@csserver2.(none)>
2011-07-25 16:46:12 +0800

    initialized Repo

--- pyqt/first_program/window.py
+++ pyqt/first_program/window.py
@@ -8,9 +8,9 @@ from PyQt4 import QtGui, QtCore
 
 
 
-class window(QtGui.QWidget):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
 
    
 			
@@ -32,10 +32,45 @@ class window(QtGui.QWidget):
         quit.move((size.width()/2) - (quitgeo.width()/2), (size.height()/2) - (quitgeo.height()/2))
         self.connect(quit, QtCore.SIGNAL('clicked()'),
             QtGui.qApp, QtCore.SLOT('quit()'))
+            
+        self.exit = QtGui.QAction(QtGui.QIcon('greyhoundz2.jpeg'), 'Exit', self)
+        self.exit.setShortcut('Ctrl+Q')
+        self.connect(self.exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(self.exit)
+
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QtGui.QMessageBox.Yes | 
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+            
+            exit = QtGui.QAction(QtGui.QIcon('greymenubar.jpeg'), 'Exit', self)
+        exit.setShortcut('Ctrl+Q')
+        exit.setStatusTip('Exit application')
+        self.connect(exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        file = menubar.addMenu('&File')
+        file.addAction(exit)
+        
+        menubar = self.menuBar()
+        file = menubar.addMenu('&Help')
+        file.addAction(exit)
+
+
 
 
 
 app = QtGui.QApplication(sys.argv)
-qb = window()
-qb.show()
+main = MainWindow()
+main.show()
 sys.exit(app.exec_())
